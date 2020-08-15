fa738d15ab75817952d385faa4709b71dd753b69
noamshuster <noam2108@gmail.com>
2020-08-15 23:20:34 +0300

    app-button update

--- src/app-button.tsx
+++ src/app-button.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-export const AppButton = () => {
+export const AppButton: React.FunctionComponent<AppButtonProps> = (props: AppButtonProps) => {
+
+    const {text, className, onButtonClick} = props;
 
     return (
-        <div>
-
-        </div>
+        <button
+            className={className}
+            onClick={onButtonClick}
+        >
+            {text}
+        </button>
     )
 }
+
+interface AppButtonProps {
+    text: string;
+    className: string;
+    onButtonClick: (event: any) => void;
+}
+
