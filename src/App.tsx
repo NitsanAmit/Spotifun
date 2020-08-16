08a4d16ec6d4fc9a62c8c35c92fa4f521e390a96
Nitsan Amit <Nitsan135@gmail.com>
2020-08-16 09:25:35 +0300

    Testing adding a pull request

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {InformationPanel} from "./info-panel";
+import {AppButton} from "./app-button";
 
 function App() {
   return (
     <div className="App">
       <InformationPanel/>
+      <AppButton label={"Click Me!"} onButtonClick={(event => console.log("Clicked!"))}/>
     </div>
   );
 }
