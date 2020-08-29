eef8c4186d43cdf17b2e3c5bc6f9412e4d0cd7ce
NitsanAmit <Nitsan135@gmail.com>
2020-08-30 00:27:36 +0300

    Update app-consts.tsx

--- src/models/app-consts.tsx
+++ src/models/app-consts.tsx
@@ -8,7 +8,7 @@ export enum AppStep{
 export const LoginConsts = {
     authEndpoint: 'https://accounts.spotify.com/authorize',
     clientId: 'f35da0c6ac834eea9ff0ea58f245244c',
-    clientSecret: '07fb41a9ede848118a02567678268d41',
+    clientSecret: CLIENT_SECRET',
     redirectUri: "http://localhost:3000",
     scopes: "user-read-currently-playing user-read-playback-state playlist-read-private playlist-read-collaborative user-top-read",
 }
